c5121769845932bf0f9c1d8dd84d07bb40d6110d
Snuzzo <jrracinfan@gmail.com>
2015-09-29 12:48:25 -0400

    Use extraversion for vanity

--- configs/common.mk
+++ configs/common.mk
@@ -1,5 +1,5 @@
 BUILD_VERSION := v0.1
 KBUILD_BUILD_USER := RenderBroken
 KBUILD_BUILD_HOST := RenderServer.net
-
+EXTRAVERSION := -RenderKernels-$(RENDER_PRODUCT)-$(BUILD_VERSION)
 PACKAGE_TARGET_NAME := RenderKernels-$(RENDER_PRODUCT)-$(BUILD_VERSION).$(shell date -u +%m.%d).zip
