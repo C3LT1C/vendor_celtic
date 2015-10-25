262e803d0cbd15b25ef46648dd8ec2fdbea46f16
Snuzzo <jrracinfan@gmail.com>
2015-10-25 03:11:27 -0400

    Add variants

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 for combo in $(curl -s https://raw.githubusercontent.com/RenderKernels/build-targets/lp5.1/products | sed -e 's/#.*$//' | awk {'print $1'})
 do
- add_lunch_combo render_$combo-kernel
+ add_lunch_combo render_$combo
 done
