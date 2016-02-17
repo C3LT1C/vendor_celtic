d9e08a5227bca898bab795d39c552c50f4e07639
Snuzzo <jrracinfan@gmail.com>
2016-02-17 04:32:56 -0500

    Use local device targets

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/RenderKernels/build-targets/mm6.0/products | sed -e 's/#.*$//' | awk {'print $1'})
+for combo in $(cat device/build/targets | sed -e 's/#.*$//' | awk {'print $1'})
 do
- add_lunch_combo render_$combo
+ add_lunch_combo black_$combo
 done
