3231fd27398c09b4497a3ac25125b58ff0b27cf9
Snuzzo <jrracinfan@gmail.com>
2016-03-18 22:53:54 -0400

    All your space belong to us

--- configs/common.mk
+++ configs/common.mk
@@ -1,3 +1,3 @@
-KBUILD_BUILD_USER := $(shell git config user.name)
+KBUILD_BUILD_USER := "$(shell git config user.name)"
 
 PACKAGE_TARGET_NAME := $(shell grep -r "EXTRAVERSION = -" $(ANDROID_BUILD_TOP)/$(PRODUCT_KERNEL_SOURCE)/Makefile | sed 's/EXTRAVERSION = -//')-$(shell git -C $(ANDROID_BUILD_TOP)/$(PRODUCT_KERNEL_SOURCE) log --pretty=format:'%h' -n 1)-$(BLACK_PRODUCT)-$(shell date -u +%m.%d).zip
