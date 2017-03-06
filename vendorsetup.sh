692e19dd237ea100d6dc943d052ace27fe40b6c8
Snuzzo <jrracinfan@gmail.com>
2017-03-05 19:14:46 -0500

    Use targets in vendor instead of placeholder file

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(cat device/build/targets | sed -e 's/#.*$//' | awk {'print $1'})
+for combo in $(cat vendor/black/device.targets | sed -e 's/#.*$//' | awk {'print $1'})
 do
  add_lunch_combo black_$combo
 done
