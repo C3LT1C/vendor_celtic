f8cb6d4c03e8b5a150156f2caa40e7df9ee83c05
REV3NT3CH <rev3nt3ch@gmail.com>
2018-03-14 17:37:56 -0700

    From Black ashes arises a C3LT1C warrior
    also update to Oreo base

--- tools/buildzip.sh
+++ tools/buildzip.sh
@@ -59,7 +59,7 @@ else
   make kernelclean
 fi
 echo "Building source. Please wait."
-if build_kernel 1> ./build-log-$BLACK_PRODUCT.log 2>&1; then
+if build_kernel 1> ./build-log-$CELTIC_PRODUCT.log 2>&1; then
   clear
   echo "Built successful"
   if [ "$TARGET_BUILD_VARIANT" != "kernel" ]; then
@@ -69,9 +69,9 @@ if build_kernel 1> ./build-log-$BLACK_PRODUCT.log 2>&1; then
   fi
 else
     echo "Errors at the following lines; Build unsuccessful"
-  for line in $(grep -n "error" ./build-log-$BLACK_PRODUCT.log | cut -f1 -d:)
+  for line in $(grep -n "error" ./build-log-$CELTIC_PRODUCT.log | cut -f1 -d:)
   do
-    cat ./build-log-$BLACK_PRODUCT.log | sed "${line}!d"
+    cat ./build-log-$CELTIC_PRODUCT.log | sed "${line}!d"
   done
-  echo "Check ./build-log-$BLACK_PRODUCT.log for verbosity"
+  echo "Check ./build-log-$CELTIC_PRODUCT.log for verbosity"
 fi
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(cat vendor/black/device.targets | sed -e 's/#.*$//' | awk {'print $1'})
+for combo in $(cat vendor/celtic/device.targets | sed -e 's/#.*$//' | awk {'print $1'})
 do
- add_lunch_combo black_$combo
+ add_lunch_combo celtic_$combo
 done
